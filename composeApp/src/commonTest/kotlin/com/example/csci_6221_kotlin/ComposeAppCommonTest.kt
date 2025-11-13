ffa9e0e288fcff263f9c0d0488193f3f6c1b2fe6
Rachit Das <rachit1031@gmail.com>
2025-11-13 18:59:11 -0500

    initial setup

---
+++ composeApp/src/commonTest/kotlin/com/example/csci_6221_kotlin/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.example.csci_6221_kotlin
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/example/csci_6221_kotlin/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.csci_6221_kotlin
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
